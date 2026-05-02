3c974e3730d1ff40fd8cff86261ff8172b37f22d
senda moussaoui <email@gmail.com>
2026-05-02 21:32:59 +0200

    Add cas3 address_student (Student -> Address)

--- src/main/java/edu/isgb/school/entities/Student.java
+++ src/main/java/edu/isgb/school/entities/Student.java
@@ -1,4 +1,4 @@
-package edu.isgb.school.entities;
+package edu.isgb.school.entities.cas1;
 
 import jakarta.persistence.*;
 import java.util.Date;
@@ -19,47 +19,7 @@ public class Student {
     @Column(name = "cl_birthdate")
     private Date birthDate;
 
-    // OWNER SIDE → FK in student table
-    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    @JoinColumn(name = "address_id", referencedColumnName = "pk_address")
-    private Address address;
-
     public Student() {}
 
-    public Student(String name, Date birthDate) {
-        this.name = name;
-        this.birthDate = birthDate;
-    }
-
-    public Integer getIdStudent() {
-        return idStudent;
-    }
-
-    public void setIdStudent(Integer idStudent) {
-        this.idStudent = idStudent;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Date getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(Date birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public Address getAddress() {
-        return address;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }
+    // getters & setters
 }
